206d84dccfd69f3c55a1418a907b3edba282972b
alfred sang <i5ting@126.com>
2015-02-07 16:56:32 +0800

    code format

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ var subl = '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl';
 var argv = process.argv;
 argv.shift();
 
-if(argv.length > 1){
-  var a = argv[0];
-  if(a == '-h' || a == '--help'){
+if ( argv.length > 1 ) {
+  var first_arg = argv[0];
+  if ( first_arg == '-h' || first_arg == '--help' ) {
     // execFile: executes a file with the specified arguments
-    child_process.execFile(subl, [], function(error, stdout, stderr){
+    child_process.execFile(subl, [], function (error, stdout, stderr) {
     	console.log(stdout);
     });
   }
   
-  if(a == '.'){
+  if ( first_arg == '.' ) {
     argv = [__dirname];
   }
 }
 // execFile: executes a file with the specified arguments
-child_process.execFile(subl, argv, function(error, stdout, stderr){
+child_process.execFile(subl, argv, function (error, stdout, stderr) {
 	console.log(stdout);
 });
